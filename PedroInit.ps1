edf0fb6809bcc723aa3678e383dff09da3912901
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2022-08-12 23:06:37 +0200

    before dzialka

--- PedroInit.ps1
+++ PedroInit.ps1
@@ -3,3 +3,4 @@
 Import-Module "$PSScriptRoot\PSModules\PedroUtilities.psm1" -Force
 Import-Module "$PSScriptRoot\PSModules\Shortcuts.psm1" -Force
 Import-Module "$PSScriptRoot\PSModules\Write-ProgressPlus.psm1"-Force
+Import-Module "$PSScriptRoot\PSModules\PedroElevation.psm1"-Force
